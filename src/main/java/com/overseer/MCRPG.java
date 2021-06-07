6c7250e3ed6c84c4106c3d530ce2d3da3fd95864
BuildTools <unconfigured@null.spigotmc.org>
2021-06-07 11:52:50 +0900

    Update

--- src/main/java/com/overseer/MCRPG.java
+++ src/main/java/com/overseer/MCRPG.java
@@ -1,5 +1,7 @@
 package com.overseer;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,12 +15,14 @@ import org.bukkit.scoreboard.Scoreboard;
 public final class MCRPG extends JavaPlugin implements Listener {
 
     Scoreboard Board = Bukkit.getScoreboardManager().getMainScoreboard();
+    Objective LV = Board.getObjective("Level");
     Objective HP = Board.getObjective("Health");
     Objective Str = Board.getObjective("Strength");
-    Objective Int = Board.getObjective("Strength");
+    Objective Int = Board.getObjective("Intelligence");
 
-    public void getScore(Player p, Objective o) {
-        o.getScore(p);
+    public void setScore(Player p, Objective o, int i) {
+        Score Score = o.getScore(p);
+        Score.setScore(i);
     }
 
     @Override
@@ -36,6 +40,11 @@ public final class MCRPG extends JavaPlugin implements Listener {
         Player p = e.getPlayer();
         if (p.hasPlayedBefore()) return;
         e.setJoinMessage(p.getName() + "님이 서버에 처음 접속하셨습니다!");
-
+        setScore(p, LV, 1);
+        setScore(p, HP, 100);
+        setScore(p, Str, 10);
+        setScore(p, Int, 10);
+        String message = "Lv. " + LV;
+        p.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
     }
 }
